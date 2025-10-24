5642535afef9d13ed7d6cef2349dea897bab2601
MrDodo9RIDERR <electromans9999@gmail.com>
2025-10-24 15:08:42 +0200

    updates installer to check for existing files

--- installer.lua
+++ installer.lua
@@ -2,13 +2,43 @@ local WGET = "wget"
 local URL = "https://raw.githubusercontent.com/doricoding/turtles/refs/heads/main/"
 local FILENAME = "structure.json"
 
-shell.run(WGET .. " " .. URL .. FILENAME)
+local function getFiles()
+    shell.run(WGET .. " " .. URL .. FILENAME)
 
-local file = fs.open(FILENAME, "r")
-local structure = textutils.unserialiseJSON(file.readAll())
-file.close()
+    local file = fs.open(FILENAME, "r")
+    local structure = textutils.unserialiseJSON(file.readAll())
+    file.close()
 
-for i, v in ipairs(structure) do
-    local command = WGET .. " " .. URL .. v .. " " .. v
-    shell.run(command)
+    for i, v in ipairs(structure) do
+        local command = WGET .. " " .. URL .. v .. " " .. v
+        shell.run(command)
+    end
+end
+
+shell.run("set motd.enable false")
+
+local rootFiles = fs.list("/")
+
+if #rootFiles-1 > 0 then
+    print("Files found in root")
+    while true do
+        print("Type (Y/N) to delete all found files on the computer")
+        local event, key, is_held = os.pullEvent("key")
+        if key == keys.y or key == keys.n then
+            if key == keys.y then
+                for i, v in ipairs(rootFiles) do
+                    if not fs.isReadOnly(v) then
+                        fs.delete(v)
+                    end
+                end
+                getFiles()
+            else
+                print("This might cause issues")
+                getFiles()
+            end
+            break
+        end
+    end
+else
+    getFiles()
 end
