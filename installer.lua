27f509091f8ec8b43f1bf1468a59769c45319bee
MrDodo9RIDERR <electromans9999@gmail.com>
2025-10-24 14:33:03 +0200

    clean stuff

--- installer.lua
+++ installer.lua
@@ -8,12 +8,7 @@ local file = fs.open(FILENAME, "r")
 local structure = textutils.unserialiseJSON(file.readAll())
 file.close()
 
-local tmp = {}
-
 for i, v in ipairs(structure) do
     local command = WGET .. " " .. URL .. v .. " " .. v
-    table.insert(tmp, command)
-    --shell.run(command)
+    shell.run(command)
 end
-
-textutils.pagedPrint(tmp)
