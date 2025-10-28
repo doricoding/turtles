3fc2009ffcc5b7dbcede7b89d5376c2b61aa63de
DoriGAMES <doricoding@gmail.com>
2025-10-28 21:37:20 +0100

    Added mixins, added round() to math, small optimalisation when creating new image instance, made data private in image

--- api/image.lua
+++ api/image.lua
@@ -1,18 +1,15 @@
--- Helper functions
-local function round(num)
-	return math.floor(num + 0.5);
-end
+-- Helper function
 local function fillRow(image, from, to, y, color)
-	y = round(y);
+	y = math.round(y);
 	if y < 1 or y > image.height then
 		return;
 	end
 
-	from = math.max(round(from), 1);
-	to = math.min(round(to), image.width);
+	from = math.max(math.round(from), 1);
+	to = math.min(math.round(to), image.width);
 
 	for x = from, to do
-		image.data[y][x] = color;
+		image._data[y][x] = color;
 	end
 end
 
@@ -27,7 +24,7 @@ local function createImageInstance()
 		width = 0,
 		height = 0,
 		-- color_format = 0,
-		data = {}
+		_data = {}
 	};
 
 	-- Load (todo)
@@ -40,9 +37,9 @@ local function createImageInstance()
 		new_image.color_format = self.color_format;
 		-- Deep copy data
 		for y = 1, self.height do
-			new_image.data[y] = {};
+			new_image._data[y] = {};
 			for x = 1, self.width do
-				new_image.data[y][x] = self.data[y][x];
+				new_image._data[y][x] = self._data[y][x];
 			end
 		end
 
@@ -58,12 +55,12 @@ local function createImageInstance()
 	-- end
 	-- Draw
 	function image:pixel(x, y, color)
-		x, y = round(x), round(y);
+		x, y = math.round(x), math.round(y);
 		if x < 1 or y < 1 or x > self.width or y > self.height then
 			return;
 		end
 
-		self.data[y][x] = color;
+		self._data[y][x] = color;
 	end
 	function image:line(x1, y1, x2, y2, color)
 		local dx = x2 - x1;
@@ -94,24 +91,24 @@ local function createImageInstance()
 			y1, y2 = y2, y1;
 		end
 		-- Clamp start positions
-		x1 = math.max(round(x1), 1);
-		y1 = math.max(round(y1), 1);
+		x1 = math.max(math.round(x1), 1);
+		y1 = math.max(math.round(y1), 1);
 		-- Return early if possition is out of bounds
 		if x1 >= self.width or y1 >= self.height then
 			return;
 		end
 		-- Clamp end positions
-		x2 = math.min(round(x2), self.width);
-		y2 = math.min(round(y2), self.height);
+		x2 = math.min(math.round(x2), self.width);
+		y2 = math.min(math.round(y2), self.height);
 
 		for y = y1, y2 do
 			if fill or y == y1 or y == y2 then
 				for x = x1, x2 do
-					self.data[y][x] = color;
+					self._data[y][x] = color;
 				end
 			else
-				self.data[y][x1] = color;
-				self.data[y][x2] = color;
+				self._data[y][x1] = color;
+				self._data[y][x2] = color;
 			end
 		end
 	end
@@ -165,7 +162,7 @@ local function createImageInstance()
 		end
 	end
 	function image:drawImage(image, x, y, dx, dy, sx, sy)
-		x, y = round(x), round(y);
+		x, y = math.round(x), math.round(y);
 		dx, dy = math.floor(dx), math.floor(dy);
 		assert(dx >= 1, "Draw image delta x can't be nagative!");
 		assert(dy >= 1, "Draw image delta y can't be nagative!");
@@ -174,13 +171,13 @@ local function createImageInstance()
 		end
 		-- Start position
 		if sx ~= nil then
-			sx = round(sx);
+			sx = math.round(sx);
 			assert(sx >= 1, "Draw image starting x offset can't be negative!");
 		else
 			sx = 1;
 		end
 		if sy ~= nil then
-			sy = round(sy);
+			sy = math.round(sy);
 			assert(sx >= 1, "Draw image starting y offset can't be negative!");
 		else
 			sy = 1;
@@ -190,9 +187,9 @@ local function createImageInstance()
 		local y_start = math.max(-(y-1), 0);
 		for i = y_start, math.min(image.height-sy, dy-1, self.height-y) do
 			for j = x_start, math.min(image.width-sx, dx-1, self.width-x) do
-				local color = image.data[sy+i][sx+j];
+				local color = image._data[sy+i][sx+j];
 				if color ~= nil then
-					self.data[y+i][x+j] = color;
+					self._data[y+i][x+j] = color;
 				end
 			end
 		end
@@ -222,9 +219,11 @@ function create(width, height, bg_color)
 
 	-- Fill image with color
 	for y = 1, height do
-		image.data[y] = {};
-		for x = 1, width do
-			image.data[y][x] = bg_color;
+		image._data[y] = {};
+		if bg_color ~= nil then
+			for x = 1, width do
+				image._data[y][x] = bg_color;
+			end
 		end
 	end
 
--- startup.lua
+++ startup.lua
@@ -1,4 +1,5 @@
 local HOME_DIR = "/home";
+local MIXIN_DIR = "/api/mixin";
 local API_DIR = "/api";
 local AUTORUN_DIR = "/autorun";
 
@@ -6,12 +7,22 @@ local apisToUnload = {
     "help",
     "rednet"
 };
-
 -- Unload default APIs
-for _, api in ipairs(apisToUnload) do os.unloadAPI(api); end
+for _, api in ipairs(apisToUnload) do
+	os.unloadAPI(api);
+end
 
+-- Load mixins
+for _, file in ipairs(fs.list(MIXIN_DIR)) do
+	shell.execute(MIXIN_DIR.."/"..file);
+end
 -- Load custom APIs
-for _, file in ipairs(fs.list(API_DIR)) do os.loadAPI(API_DIR.."/"..file); end
+for _, file in ipairs(fs.list(API_DIR)) do
+	local path = API_DIR.."/"..file;
+	if not fs.isDir(path) then
+		os.loadAPI(path);
+	end
+end
 
 -- Run Autorun scripts
 if fs.exists(AUTORUN_DIR) then
