d322aa480d798e143992699b60fd1c6d288fb8ee
DoriGAMES <doricoding@gmail.com>
2025-11-13 11:47:45 +0100

    Formatting correction

--- service/celestial_manipulator_timeskip.lua
+++ service/celestial_manipulator_timeskip.lua
@@ -5,12 +5,12 @@ local skipTimeTrigger = 18.5;
 local use24hourFormat = true
 
 while true do
-    local time = os.time("ingame");
-    if time > skipTimeTrigger then
-        print(string.format("Skipping time at %s", textutils.formatTime(os.time("utc") + timeOffset, use24hourFormat)));
-        redstone.setOutput(SIDE_CELESTIAL_MANIP, true);
-        sleep(1);
-        redstone.setOutput(SIDE_CELESTIAL_MANIP, false);
-        sleep(60);
-    end
+	local time = os.time("ingame");
+	if time > skipTimeTrigger then
+		print(string.format("Skipping time at %s", textutils.formatTime(os.time("utc") + timeOffset, use24hourFormat)));
+		redstone.setOutput(SIDE_CELESTIAL_MANIP, true);
+		sleep(1);
+		redstone.setOutput(SIDE_CELESTIAL_MANIP, false);
+		sleep(60);
+	end
 end
